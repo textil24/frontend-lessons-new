29362841231d3137a50ecb515de17ca9e1c0ff5c
textil24 <textilofficial@mail.ru>
2023-07-25 11:27:26 +0300

    Изменил 50px на padding

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import Course from "./pages/Course"
 const App = () => {
   return (
     <Container my={4} maxW='1200'>
-      <Box marginTop={"90px"} marginBottom={"50px"}>
+      <Box marginTop={"90px"} paddingBottom={"50px"} bgColor={"red"}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/course" element={<Course />} />
