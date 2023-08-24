a62560c63369fb6ff28819ebf5427e8857af9b4d
textil24 <textilofficial@mail.ru>
2023-08-24 17:30:59 +0300

    Сделал заготовку под CKEditor

--- src/components/Admin/Editor/Editor.tsx
+++ src/components/Admin/Editor/Editor.tsx
@@ -2,20 +2,32 @@ import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { useState } from 'react';
 import { SelectionWrapper } from '..';
+import { useForm } from 'react-hook-form';
 
 const Editor = () => {
     const [editor, setEditor] = useState("Hello from CKEditor&nbsp;5!")
 
+    const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm<{ editor: string }>()
+
+    const onSubmit = (data: any) => {
+        console.log(data)
+        if (data.title) {
+            console.log(data)
+        }
+    };
+
     return (
         <SelectionWrapper name='Текстовый редактор'>
-            <CKEditor
-                editor={ClassicEditor}
-                data="<p>Hello from CKEditor&nbsp;5!</p>"
-                onChange={(event, editor) => {
-                    const data = editor.getData();
-                    setEditor(data)
-                }}
-            />
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <CKEditor
+                    editor={ClassicEditor}
+                    // data="<p>Hello from CKEditor&nbsp;5!</p>"
+                    onChange={(event, editor) => {
+                        const data = editor.getData();
+                        setEditor(data)
+                    }}
+                />
+            </form>
         </SelectionWrapper>
     )
 }
