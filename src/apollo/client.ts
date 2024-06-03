2275c908424e1206db57cf726ac6e9dc0a84ad8f
textil24 <textilofficial@mail.ru>
2024-06-03 22:16:22 +0300

    update

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://95.213.248.225:4001/',
+  uri: 'https://souzmultapi.ru/api',
   cache: new InMemoryCache()
 });
 
